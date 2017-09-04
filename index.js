318756ea375a109f2c7414b545b715197feb7fbf
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2017-09-04 19:46:41 +0530

    Use passport-heroku instead of manually inputting key

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var uuidV4 = require('uuid/v4');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const uuidV4 = require('uuid/v4');
+const passport = require("passport");
+const crypto = require('crypto');
+var session = require("express-session");
+
+const HerokuStrategy = require("passport-heroku").Strategy;
+
+const HEROKU_CLIENT_ID = process.env.HEROKU_CLIENT_ID || 'sdfdsgsd';
+const HEROKU_CLIENT_SECRET = process.env.HEROKU_CLIENT_SECRET || 'sdfgsf';
+const cipher = crypto.createCipher('aes256', process.env.SECRET);
+const decipher = crypto.createDecipher('aes256', process.env.SECRET);
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new HerokuStrategy({
+    clientID: HEROKU_CLIENT_ID,
+    clientSecret: HEROKU_CLIENT_SECRET,
+    callbackURL: "https://herokuception.herokuapp.com/auth/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    profile.accessToken = accessToken;
+    done(null, profile);
+  }
+));
+
+app.set('views', __dirname);
+app.set('view engine', 'hbs');
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: true,
+  store: new session.MemoryStore()
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.render('index', {
+    herokuAPIKey: req.session.herokuAPIKey || ''
+  });
+});
+
+app.post('/auth', passport.authenticate('heroku'));
+
+app.get('/auth/callback', passport.authenticate('heroku'), function(req, res) {
+  req.session.herokuAPIKey = cipher.update(req.user.accessToken, 'utf8', 'hex') + cipher.final('hex');
+  res.redirect('/');
 });
 
 io.on('connection', function(socket){
@@ -19,7 +69,7 @@ var executeScript = function (socket, data) {
   var email = data.email;
   var gitUrl = data.gitUrl;
   var branchName = data.branchName;
-  var herokuAPIKey = data.herokuAPIKey;
+  var herokuAPIKey = decipher.update(data.herokuAPIKey, 'hex', 'utf8') + decipher.final('utf8');
   var appName = data.appName;
   var uniqueId = uuidV4();
 
